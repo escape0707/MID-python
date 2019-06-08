7d06e917cc6d937b1b998d1b99793467132f5100
Jay Chu <tothesong@gmail.com>
2019-06-08 22:03:13 +0800

    train_model() & main()

--- model.py
+++ model.py
@@ -2,6 +2,9 @@
 import time
 import copy
 
+# functions to show an image
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 from torch import nn, optim
 from torchvision.utils import make_grid
@@ -32,26 +35,29 @@ def decode_block(in_f, out_f):
 class CNNDAE(nn.Module):
     def __init__(self):
         super(CNNDAE, self).__init__()
-        
+
         self.encoder = nn.Sequential(
             encode_block(1, 64),
             encode_block(64, 64)
         )
-        
+
         self.decoder = nn.Sequential(
             decode_block(64, 64),
             decode_block(64, 64),
             nn.Conv2d(64, 1, 5, padding=2),
         )
-        
+
     def forward(self, x):
         x = self.encoder(x)
         x = self.decoder(x)
         return x
-    
+
 # Model training procedure
 def train_model(model, dataloaders, criterion, optimizer, num_epochs=iteration_times):
+
     since = time.time()
+    # Array for loss history of training and validating
+    loss_history = {'train': [], 'val': []}
 
     # Train model by loop over the dataset multiple times
     for epoch in range(num_epochs):
@@ -73,86 +79,59 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=iteration_t
                 # Send inputs to correct device
                 noisy, original = noisy.to(device), original.to(device)
 
-            # Zero the parameter gradients
-            optimizer.zero_grad()
+                # Zero the parameter gradients
+                optimizer.zero_grad()
 
                 # Forward and track history if only in train phase
                 with torch.set_grad_enabled(phase == 'train'):
                     # Get model outputs and calculate loss
                     outputs = model(noisy)
-            loss = criterion(outputs, original)
+                    loss = criterion(outputs, original)
 
                 # Backward + optimize only if in training phase
                 if phase == 'train':
-            loss.backward()
-            optimizer.step()
+                    loss.backward()
+                    optimizer.step()
 
                 # Statistics
                 running_loss += loss.item() * noisy.size(0)
-            
+
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             print('{} Loss: {:.4f}'.format(phase, epoch_loss))
             loss_history[phase].append(epoch_loss)
-    
+
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-            
-        # Print statistics
-        print('[%d] training loss: %.10d' %
-            (epoch + 1, training_loss))
-        
-        # Set loss counter for validating
-        valid_loss = 0.0
-        for i, data in enumerate(testloader):
-            # Get the inputs
-            noisy, original = data[0].to(device), data[1].to(device)
 
-            # Forward
-            outputs = net(noisy)
-            loss = criterion(outputs, original)
-
-            # Sum loss
-            valid_loss += loss.item()
-            
-        # Print statistics
-        print('[%d] validating loss: %.10d' %
-            (epoch + 1, valid_loss))
-
-    print('Finised training.')
+    return model, loss_history
 
 
+def main():
+    # Detect and use GPU acceleration when possible
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# functions to show an image
-import matplotlib.pyplot as plt
-import numpy as np
+    # Assuming that we are on a CUDA machine, this should print a CUDA device:
+    print(device)
 
-def sample_show(images, labels):
-    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(20, 4))
-    image = make_grid(images, nrow=batch_size)
-    label = make_grid(labels, nrow=batch_size)
-    image = image / 2 + 0.5     # unnormalize
-    label = label / 2 + 0.5     # unnormalize
-    npimage = image.numpy()
-    nplabel = label.numpy()
-    ax[0].imshow(np.transpose(npimage, (1, 2, 0)))
-    ax[1].imshow(np.transpose(nplabel, (1, 2, 0)))
-    plt.show()
+    # Create model instance
+    model_ft = CNNDAE()
 
+    # Use optimal device to train the model
+    model_ft.to(device)
 
-#%%
-dataiter = iter(testloader)
-images, labels = dataiter.next()
+    # Print the network architecture for sanity check reason
+    print(model_ft)
 
-# print images
-print(images.shape)
-sample_show(images, labels)
+    # Dataloaders containing trainloader and testloader for training and validating
+    dataloaders_dict = {'train': trainloader, 'val': testloader}
 
+    # Set the criterion (the loss function) to be Mean Squared Error loss
+    criterion = nn.MSELoss()
 
-#%%
-net.eval()
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = torch.clamp(net(images.to(device)).cpu(), min=-1, max=+1)
-        print(outputs.shape)
-        sample_show(outputs, labels)
+    # Set the optimizer (the optimize method) to be Adam algorithm
+    optimizer = optim.Adam(model_ft.parameters(), lr=learning_rate)
+
+    model_ft, loss_history = train_model(model_ft, dataloaders_dict, criterion, optimizer, iteration_times)
+
+if __name__ == '__main__':
+    main()

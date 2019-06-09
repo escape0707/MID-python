dd073fc88f0b9690de37c70bf009d6691f6ff772
Jay Chu <tothesong@gmail.com>
2019-06-09 18:46:24 +0800

    Add ndarray_show()

--- utils.py
+++ utils.py
@@ -2,5 +2,8 @@ import numpy as np
 
 # Function to show an tensor in specified axes
 def tensor_show(ax, tensor):
-    npimg = tensor.numpy()
-    ax.imshow(np.transpose(npimg, (1, 2, 0)))
+    ndarray = tensor.numpy()
+    ndarray_show(ax, ndarray)
+
+def ndarray_show(ax, ndarray):
+    ax.imshow(np.transpose(ndarray, (1, 2, 0)))
